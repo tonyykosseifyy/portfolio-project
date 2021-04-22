30707d1fe15fb51fb67033974543408fb8462e46
Tony Kosseify <tonykosseify123@gmail.com>
2021-04-22 20:08:18 +0300

    finish the Home Section

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { createRef , useEffect } from 'react'
 import './App.css' ;
 import './styles.sass' ;
 import video from './video.mp4' ;
-import { Title , TitleSpan , Home , MainButton} from './StyledComponents' ;
+import { Title , TitleSpan , Home , MainButton , Navbar , Link , About , Contact ,SectionTitle , Bar } from './StyledComponents' ;
+import { FaArrowRight } from 'react-icons/fa' ;
+import Fade from 'react-reveal/Fade' ;
 
 function App() {
   const videoRef = createRef(null) ;
   useEffect(() => {
       videoRef.current.play() ;
   }, [])
-
         return (
         <div className='app'>
-          <Home>
+          <Home id='home'>
             <video ref={videoRef} autoplay muted loop>
               <source src={video} />
             </video>
@@ -20,8 +21,33 @@ function App() {
                 Hello, I'm <TitleSpan color='#832D21'>Tony Kosseify.</TitleSpan> <br/>
                 I'm a React Front End Developer
               </Title>
-              <MainButton>View my Work</MainButton>
+              <MainButton href='#about'>
+                View my Work
+                <FaArrowRight />
+              </MainButton>
             </Home>
+
+            <Navbar>
+              <Link href='#home'>Home</Link>
+              <Link href='#about'>About</Link>
+              <Link href='#projects'>Projects</Link>
+              <Link href='#contact'>Contact</Link>
+            </Navbar>
+
+            <About id='about'>
+
+            </About>
+
+            <Contact id='contact'>
+                <Fade left>
+                  <SectionTitle color='white'>
+                    Contact
+                  </SectionTitle>
+                </Fade>
+                <Fade left >
+                  <Bar color='white'/>
+                </Fade>
+            </Contact>
         </div>
 
 
--- src/StyledComponents.js
+++ src/StyledComponents.js
@@ -22,16 +22,90 @@ export const Home = styled.main`
   flex-direction: column ;
   align-items:center ;
   justify-content: center ;
-  width: 100vw ;
+  width: 100% ;
   height: 100vh ;
   z-index: 100 ;
 `
 export const MainButton = styled.a`
-  display: block ;
+  display: flex ;
+  align-items : center ;
+  justify-content: space-around ;
   color: white ;
   border: 2px solid white ;
-  padding: 10px 30px ;
+  padding: 15px 30px ;
+  width: 220px ;
   font-weight: bold ;
   z-index: 100 ;
   margin: 30px 0 ;
+  cursor: pointer ;
+  transition: .3s ease-in-out ;
+  &:hover {
+    background-color: #832D21 ;
+    border-color: #832D21 ;
+  }
+  & > svg {
+      transition: .3s ease-in-out ;
+  }
+
+  &:hover > svg {
+    transform: rotate(90deg)
+  }
+`
+export const Link = styled.a`
+  cursor: pointer ;
+  font-size: 17px ;
+  color: white ;
+  margin: 0 15px ;
+  display: flex ;
+  align-items: center ;
+  justify-content: center ;
+  min-width: 40px ;
+  padding: 0 10px ;
+  transition: .3s ease-in-out ;
+  &:hover {
+    color: #832D21;
+  }
+
+`
+
+export const Navbar = styled.nav`
+  position: sticky;
+  top: 0 ;
+  left: 0 ;
+  right: 0 ;
+  width: 100% ;
+  background-color: #1F1214;
+  min-height: 50px ;
+  border-bottom: 4px solid #832D21 ;
+  display: flex ;
+  padding-left: 20px ;
+  z-index: 10000 ;
+`
+export const About = styled.section`
+  min-height: 200vh ;
+  width: 100% ;
+
+`
+export const Contact = styled.section`
+  clip-path: polygon(0 0, 50% 25%, 100% 0, 100% 100%, 0 100%, 0% 60%, 0 0);
+  height: 500px ;
+  width: 100% ;
+  background: black ;
+  padding-top: 200px;
+`
+
+export const SectionTitle = styled.h1`
+  font-size: clamp(.8rem , 2.7rem , 4.5rem) ;
+  color: ${(props) => props.color || 'black'} ;
+  text-align: center ;
+  @media (max-width:400px) {
+        font-size: 2rem ;
+  }
+`
+
+export const Bar = styled.div`
+  width: ${(props) => props.width || '100px'} ;
+  height: 5px ;
+  background-color: ${(props) => props.color || 'white'} ;
+  margin: 8px auto ;
 `
