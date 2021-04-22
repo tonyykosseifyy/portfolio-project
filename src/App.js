5ae45f1938a73bb88195a6975654d5bb556fc004
Tony Kosseify <tonykosseify123@gmail.com>
2021-04-22 16:07:37 +0300

    add a video background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { createRef , useEffect } from 'react'
 import './App.css' ;
+import './styles.sass' ;
+import video from './video.mp4' ;
 
 
 function App() {
-    return (
+  const videoRef = createRef(null) ;
+  useEffect(() => {
+      videoRef.current.play() ;
+  }, [])
+
+        return (
         <div className='app'>
+            <video ref={videoRef} autoplay muted loop>
+              <source src={video} />
+          </video>
 
         </div>
+
+
     )
 } ;
 
