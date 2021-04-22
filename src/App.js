c3bb010875a9c8e1fc25084ec89251aa11e827fb
Tony Kosseify <tonykosseify123@gmail.com>
2021-04-22 17:12:38 +0300

    background video with a title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { createRef , useEffect } from 'react'
 import './App.css' ;
 import './styles.sass' ;
 import video from './video.mp4' ;
-
+import { Title , TitleSpan , Home , MainButton} from './StyledComponents' ;
 
 function App() {
   const videoRef = createRef(null) ;
@@ -12,10 +12,16 @@ function App() {
 
         return (
         <div className='app'>
+          <Home>
             <video ref={videoRef} autoplay muted loop>
               <source src={video} />
-          </video>
-
+            </video>
+              <Title>
+                Hello, I'm <TitleSpan color='#832D21'>Tony Kosseify.</TitleSpan> <br/>
+                I'm a React Front End Developer
+              </Title>
+              <MainButton>View my Work</MainButton>
+            </Home>
         </div>
 
 
--- src/StyledComponents.js
+++ src/StyledComponents.js
@@ -1,2 +1,37 @@
 import styled from 'styled-components' ;
 
+
+
+export const Title = styled.h1`
+  font-size: clamp(.8rem , 2.7rem , 4.5rem) ;
+  z-index: 100 ;
+  color: white ;
+  @media (max-width: 860px) {
+      text-align: center ;
+  }
+  @media (max-width:400px) {
+        font-size: 2rem ;
+  }
+`
+
+export const TitleSpan = styled.span`
+  color: ${(props) => props.color || 'inherit'}
+`
+export const Home = styled.main`
+  display: flex ;
+  flex-direction: column ;
+  align-items:center ;
+  justify-content: center ;
+  width: 100vw ;
+  height: 100vh ;
+  z-index: 100 ;
+`
+export const MainButton = styled.a`
+  display: block ;
+  color: white ;
+  border: 2px solid white ;
+  padding: 10px 30px ;
+  font-weight: bold ;
+  z-index: 100 ;
+  margin: 30px 0 ;
+`
